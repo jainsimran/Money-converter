24e73df13496348fe8650a16c42a3dfb8f111fbc
jainsimran <itsimmi5@gmail.com>
2019-12-16 20:34:55 -0500

    US dollars to CAD dollars

--- src/App.js
+++ src/App.js
@@ -1,28 +1,43 @@
 import React, { Component } from 'react';
-import Converted from './Converted';
 
 export default class App extends Component {
   
   constructor(){
     super();
     this.state= {
-      usCurr: 0
+      inputAmount: 0,
+      isConvert: false,
+      result: 0
     }
     this.convertMoney= this.convertMoney.bind(this);
   }
 
   convertMoney(){
-    let usNum = this.state.usCurr;
-    let resultnum = usNum *20;
-    <Converted result = {this.resultnum} />
+    fetch('https://api.exchangeratesapi.io/latest/')
+    .then(data => {
+      return data.json();
+    })
+    .then(result => {
+      console.log(result);
+      let resultAmount = this.state.result;
+      let inputAmt = this.state.inputAmount;
+      resultAmount = inputAmt * result.rates.CAD;
+      this.setState({ result:resultAmount.toFixed(2) })
+    })
+    .catch(err => {
+      console.log('Something bad happened!');
+    })
   }
 
   render() {
     return (
       <div>
-        <input type="text" value={this.usCurr} onChange={(e) => this.setState({usCurr: e.target.value})}></input>
-        {/* <input type="text" value={this.usCurr}></input> */}
-        <button onClick={this.convertMoney}> convert </button>
+        Enter US dollar = 
+        <input type="text" value={this.state.inputAmount} placeholder = "enter US dollars" onChange={(e) => this.setState({inputAmount: e.target.value})}></input>
+        <button onClick={this.convertMoney}> convert to CAD </button>
+
+        {this.isConvert ? <h1>Canadian dollars = {this.state.result}</h1> : <h1>Canadian dollars = {this.state.result}</h1> }
+        {/* {this.isConvert ? <h1>Canadian dollars is {this.result}</h1> : <h1> Enter values to convert</h1> } */}
       </div>
     )
   }
